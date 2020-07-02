ebf50c7a3becd4c5ac78514f4f5f35c750f2c5b9
Chandosh Kumar <chandoshkumar@gmail.com>
2020-07-02 20:24:39 +0530

    Image editor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-profile';
+  title = 'imageEditor';
+  imageLocation = 'http://www.auto-power-girl.com/high-resolution-wallpapers/audi-a3-cabriolet/audi-a3-cabriolet-2008-14.jpg';
+
+  save($event) {
+  }
+  cancel() {
+  }
 }
